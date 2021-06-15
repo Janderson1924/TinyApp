2a7753bcee690f2bf5f31fbdafa14cde09829178
Justin Anderson <justinanderson101@gmail.com>
2021-06-15 16:13:13 -0400

    cleaned and reformatted

--- helpers.js
+++ helpers.js
@@ -1,23 +1,23 @@
 const generateRandomString = () => {
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let randomString = '';
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomString = "";
   while (randomString.length < 6) {
     randomString += chars[Math.floor(Math.random() * chars.length)];
   }
   return randomString;
 };
 
-
-const getUserByEmail = function(email, users) {
+const getUserByEmail = function (email, users) {
   for (const userID in users) {
     if (email === users[userID].email) {
       return userID;
     }
   }
   return undefined;
-}
+};
 
-const urlsForUser = function(id, database) {
+const urlsForUser = function (id, database) {
   let userURL = {};
   for (let key in database) {
     if (database[key].userID === id) {
